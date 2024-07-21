34476a7b0b7c81ce03fb27dc6f3641b7b8fb41a4
eva1606 <168004475+eva1606@users.noreply.github.com>
2024-07-21 19:30:30 +0300

    Configured main Express server

---
+++ vacation-planner/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+const authRoutes = require('./routes/authRoutes');
+const preferenceRoutes = require('./routes/preferenceRoutes');
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/preferences', preferenceRoutes);
+
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}`);
+});
